7eff04eebaec8176a08f62ee4d055ed823e59b90
Aarambh Vaish <aarambhvaish1708@gmail.com>
2025-01-25 02:18:17 +0530

    Module Completed

--- fibo.py
+++ fibo.py
@@ -1,1 +1,7 @@
-print("This file pushed into git")
+def fibo(n):
+    a,b=map(int,input("Enter first two element of fibonnaci series:").split())
+    print("fibonacci series upto ", n,"Numbers is: ")
+    for _ in range(n):
+        print(a,end=" ")
+        a,b=b,b+a
+fibo(10)
