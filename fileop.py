12bbbd6699cb9fe7918e65986559e6eb06f2173f
Aarambh Vaish <aarambhvaish1708@gmail.com>
2025-01-27 02:49:44 +0530

    File operation done

--- fileop.py
+++ fileop.py
@@ -1,10 +1,10 @@
 file=open("abc.txt",'r')
-word=file.read()
-words=word.lower().split()
+word=file.read()#file.read()convert the file contents into a single string
+words=word.lower().split()#split() seprates words and automatically creates a list to store them
 final=[]
 for i in words:
-    final.append(i.strip(".,!?;:'\"()[]{}"))
-unique=sorted(set(final))
+    final.append(i.strip(".,!?;:'\"()[]{}"))#strip() removes desired words from the file
+unique=sorted(set(final))#set() convers the list in the set so that no element is repeated
 print("Unique words in file arranged alphabetically are")
-for i in unique:
-    print(i)
+for j in unique:
+    print(j)
